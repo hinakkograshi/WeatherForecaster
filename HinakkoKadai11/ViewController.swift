5e33afad47155c5f3e5e2e7b7961e005279f505c
hinakko <131275914+hinakko02@users.noreply.github.com>
2023-06-26 23:27:46 +0900

    First Commit

---
+++ HinakkoKadai11/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  HinakkoKadai11
+//
+//  Created by Hina on 2023/06/26.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
